ca81a0ecb5486d1f63df15f6955d1700ca74694c
prajwalbhatia <prajwal6bhatia@gmail.com>
2021-07-04 20:31:52 +0530

    Reward page done

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -11,7 +11,7 @@ function Header(props)
     return (
         <header className="header">
             <div className="header-text-container">
-                <h2>{props.headerText}</h2>
+                <h1>{props.headerText}</h1>
                 <IconContext.Provider value={{ style: {fontSize: '1.2rem' , marginTop : '6px' , marginLeft : '5px' , cursor : 'pointer'}}}>  {props.icon} </IconContext.Provider>
             </div>
             {
--- src/pages/Rewards/rewards.jsx
+++ src/pages/Rewards/rewards.jsx
@@ -1,13 +1,105 @@
 import React from 'react';
 
+//CSS
+import "./rewards.css";
+
+import { AiFillTrophy } from "react-icons/ai";
+import {IconContext} from "react-icons"
+
+
 //COMPONENTS
 import Navigation from "../../components/navigation/navigation";
+import Header from "../../components/header/header";
+import Card from "../../components/card/card";
+import Modal from "../../components/modal";
+
 
 function Streak(props) {
+
+  const dialog = () => {
+    Modal.show({
+      title : 'Create reward',
+      icon : <AiFillTrophy/>,
+      primaryButtonText : 'Create',
+      secondaryButtonText : 'Cancel',
+      content : 
+      [
+        {
+          label : 'Title',
+          uid : 'title',
+          type : 'text',
+          eleType : 'input'
+        },
+        {
+          label : 'Add to streak',
+          uid : 'add',
+          type : 'text',
+          eleType : 'input'
+        },
+        {
+          label : 'Date',
+          uid : 'date',
+          type : 'date',
+          eleType : 'input'
+        },
+        
+    ],
+      btnClickHandler : (data) => {
+          console.log(data)
+          if(data.type === 'secondary')
+          Modal.hide()
+      }
+    });
+  }
+
+
   return (
-    <div>
+    <div className="rewards">
        <Navigation />
-      <h1>REWARDS</h1>
+       <div className="rewards-main-container">
+          {/* Header */}
+          <div className="pad-global">
+              <Header
+                  headerText={'Rewards'}
+                  withSearchBox={false}
+              />
+          </div>
+
+          <div className="rewards-area">
+            <div className="left-portion">
+              <h4>TO BUY</h4>
+
+              <div className="rewards-list">
+                <Card withLine={true} cardClass="streak-card">
+                  <h4>100 days of Javascript</h4>
+                  <p className="mt-1">The motive of this streak is to keep learning JS with consistency....</p>
+                  
+                </Card>
+
+                <Card cardClass="new-reward-card" onClick={() => dialog()}>
+                  <div className="content-container">
+                    <h5>Create new reward</h5>
+                    <IconContext.Provider 
+                      value={{ style: {fontSize: '1rem', color: "var(--primaryColor)" , marginTop : "1px" , marginRight : "5px"}}}>
+                      <AiFillTrophy/>
+                    </IconContext.Provider>
+                  </div>
+              </Card>
+              </div>
+            </div>
+            <div className="right-portion">
+              <h4>Rewards Earned</h4>
+
+              <div className="rewards-list">
+                <Card withLine={true} cardClass="streak-card">
+                  <h4>100 days of Javascript</h4>
+                  <p className="mt-1">The motive of this streak is to keep learning JS with consistency....</p>
+                  
+                </Card>
+              </div>
+            </div>
+          </div>
+       </div>
     </div>
   );
 }
