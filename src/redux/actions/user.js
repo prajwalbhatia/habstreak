295988b82a4b04d2768be43b403af83fc664df40
Prajwal <prajwal6bhatia@gmail.com>
2022-02-03 22:52:59 +0530

    NEW : Authentication with email and password

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -1,7 +1,9 @@
 import { AUTH, LOGOUT } from '../constants/action-type';
 
 import {
-    createUser
+    createUser,
+    signIn,
+    signUp
 } from '../api/user';
 
 export const auth = (data) => async (dispatch) => {
@@ -9,6 +11,28 @@ export const auth = (data) => async (dispatch) => {
         await createUser(data.result);
         const action = { type: AUTH, data }
         dispatch(action);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const signin = (formData, history) => async dispatch => {
+    try {
+        const { data } = await signIn(formData);
+        const action = { type: AUTH, data }
+        dispatch(action);
+        history.push('/dashboard');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const signup = (formData, history) => async dispatch => {
+    try {
+        const { data } = await signUp(formData);
+        const action = { type: AUTH, data }
+        dispatch(action);
+        history.push('/dashboard');
     } catch (error) {
         console.log(error);
     }
